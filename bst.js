58e62e6ae1501f1c5b1d58df78c213f4d3b9fe63
Amina <minassouane@gmail.com>
2026-01-30 17:12:25 +0100

    Add insert function

--- bst.js
+++ bst.js
@@ -26,12 +26,36 @@ class Tree {
   buildTree(array) {
     const sortedArray = [...new Set(array)].sort((a, b) => a - b);
 
-    return this.bstRecur(sortedArray, 0, array.length - 1);
+    return this.bstRecur(sortedArray, 0, sortedArray.length - 1);
+  }
+
+  insert(value) {
+    const temp = new Node(value);
+
+    // If tree is empty
+    if (this.root === null) {
+      this.root = temp;
+      return;
+    }
+
+    // Find the node who is going to have the new node as its child
+    let curr = this.root;
+    while (curr !== null) {
+      if (curr.data === value) return;
+      if (curr.data > value && curr.left !== null) {
+        curr = curr.left;
+      } else if (curr.data < value && curr.right !== null) {
+        curr = curr.right;
+      } else break;
+    }
+
+    // If key is smaller, make it left child, else right child
+    if (curr.data > value) curr.left = temp;
+    else curr.right = temp;
   }
 }
 
-const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-
+// TESTS
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
     return;
@@ -45,4 +69,9 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+prettyPrint(tree.root);
+
+tree.insert(100);
 prettyPrint(tree.root);
