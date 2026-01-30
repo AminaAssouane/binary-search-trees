3f099a17e58c31007d40dc6645d3417b45a9c878
Amina <minassouane@gmail.com>
2026-01-30 05:18:38 +0100

    Add BST creation logic

--- bst.js
+++ bst.js
@@ -1,7 +1,48 @@
 class Node {
-  constructor(data, left = null, right = null) {
+  constructor(data) {
     this.data = data;
-    this.left = left;
-    this.right = right;
+    this.left = null;
+    this.right = null;
   }
 }
+
+class Tree {
+  constructor(array) {
+    this.root = this.buildTree(array);
+  }
+
+  bstRecur(arr, start, end) {
+    if (start > end) return null;
+
+    let mid = Math.floor((start + end) / 2);
+    let root = new Node(arr[mid]);
+
+    root.left = this.bstRecur(arr, start, mid - 1);
+    root.right = this.bstRecur(arr, mid + 1, end);
+
+    return root;
+  }
+
+  buildTree(array) {
+    const sortedArray = [...new Set(array)].sort((a, b) => a - b);
+
+    return this.bstRecur(sortedArray, 0, array.length - 1);
+  }
+}
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+prettyPrint(tree.root);
